b11b4a5a945710d318d61d2bf8ae9b850256a36f
elyak123 <elyak.123@gmail.com>
2016-03-04 06:27:30 -0600

    prueba

--- Templates/encabezado.js
+++ Templates/encabezado.js
@@ -1,21 +1,23 @@
-$.get('http://elyak123.github.io/Portafolio-fcc/Templates/encabezado.html', function (data){
-	$('header').html(data);
-});
-$(".boton-movil").click(function(){
-	debugger;
-	if ($("li").hasClass("menu-listitem")) {
-		console.log("true");
-		$(".menu-listitem").slideDown("slow").addClass("menu-listMovil").removeClass("menu-listitem");	
-	} else{
-		console.log("false");
-		$(".menu-listMovil").slideUp("slow").addClass("menu-listitem");
-		//$(".menu-listitem").removeClass("menu-listMovil");
-	};
-	
-	console.log("click");
-	$(window).on("resize", function () {
-		if ($(window).width() > 500) {
-    		$("li").css('display','').removeClass("menu-listMovil");
-		}
-	}).resize();
-});
+function encabezado (){
+	$.get('http://elyak123.github.io/Portafolio-fcc/Templates/encabezado.html', function (data){
+		$('header').html(data);
+	});
+	$(document).on('click', '.boton-movil', function(){
+		debugger;
+		if ($("li").hasClass("menu-listitem")) {
+			console.log("true");
+			$(".menu-listitem").slideDown("slow").addClass("menu-listMovil").removeClass("menu-listitem");	
+		} else{
+			console.log("false");
+			$(".menu-listMovil").slideUp("slow").addClass("menu-listitem");
+			//$(".menu-listitem").removeClass("menu-listMovil");
+		};
+		
+		console.log("click");
+		$(window).on("resize", function () {
+			if ($(window).width() > 500) {
+	    		$("li").css('display','').removeClass("menu-listMovil");
+			}
+		}).resize();
+	});
+}
