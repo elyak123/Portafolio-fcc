fd15af42ec9cf778750e1f3bbac2cf66d6fd9677
elyak123 <elyak.123@gmail.com>
2016-03-10 05:30:27 -0600

    Se agrega footer como template

--- Templates/encabezado.js
+++ Templates/encabezado.js
@@ -4,19 +4,20 @@ function encabezado (){
 	});
 	$(document).on('click', '.boton-movil', function(){
 		if ($("li").hasClass("menu-listitem")) {
-			console.log("true");
 			$(".menu-listitem").slideDown("slow").addClass("menu-listMovil").removeClass("menu-listitem");	
 		} else{
-			console.log("false");
 			$(".menu-listMovil").slideUp("slow").addClass("menu-listitem");
 			//$(".menu-listitem").removeClass("menu-listMovil");
 		};
-		
-		console.log("click");
 		$(window).on("resize", function () {
 			if ($(window).width() > 500) {
 	    		$("li").css('display','').removeClass("menu-listMovil");
 			}
 		}).resize();
+	});	
+}
+function footer(){
+	$.get('http://elyak123.github.io/Portafolio-fcc/Templates/footer.html', function(data){
+		$('body')append(data);
 	});
 }
